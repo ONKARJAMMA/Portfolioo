2497e4ff0a543c611ab7b25e49d27930e937033c
onkarjamma <122007046+ONKARJAMMA@users.noreply.github.com>
2025-04-02 18:24:12 +0530

    onkar jamma portfolio

---
+++ src/utils/Themes.js
@@ -0,0 +1,22 @@
+export const darkTheme = {
+    bg:"#1C1C27",
+    bgLight: "#1C1E27",
+    primary:"#854CE6",
+    text_primary:"#F2F3F4",
+    text_secondary:"#b1b2b3",
+    card:"#171721",
+    card_light: '#191924',
+    button:"#854CE6",
+    white:"#FFFFFF",
+    black:"#000000",
+}
+
+export const lightTheme = {
+    bg: "#F9F9F9",  // Light gray background
+    bgLight: "#EAEAEA",  // Softer light gray
+    primary: "#007BFF",  // Bright blue accent
+    text_primary: "#202124",  // Dark text for readability
+    text_secondary: "#5F6368",  // Gray secondary text
+    card: "#FFFFFF",  // White card background
+    button: "#007BFF",  // Blue button
+};
